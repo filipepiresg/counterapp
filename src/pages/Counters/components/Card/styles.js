7932dd5cb0abb0483017e5766091c1531926fd1e
Filipe Pires <filipepiresg@gmail.com>
2021-02-13 05:17:58 -0300

    style: decrease opacity in onpress

--- src/pages/Counters/components/Card/styles.js
+++ src/pages/Counters/components/Card/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components/native';
 import { Colors } from '~/styles';
 
 export const Container = styled.TouchableOpacity.attrs({
-  activeOpacity: 0.8,
+  activeOpacity: 1,
 })`
   width: 100%;
   background-color: ${(props) => (props.isActive ? Colors.CARD_ACTIVE : Colors.CARD_INACTIVE)};
